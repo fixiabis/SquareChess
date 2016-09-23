b86091427f2ea0d7dcfbb1f2d7f521374b6bd8bc
fixiabis <benkao0000@gmail.com>
2016-09-23 23:06:47 +0800

    Add files via upload

--- 7.0/Shell/Divider-Zombie.js
+++ 7.0/Shell/Divider-Zombie.js
@@ -1,11 +1,12 @@
+Sqr[1]=["Z","red","black"]
 function ExR(){var zb=[];if(Turn<10)return
 	for(cd1=65;cd1<74;cd1++)for(cd2=1;cd2<10;cd2++){
-		var c=Chr(cd1)+cd2;if(Qre(c,"T")==" "||Qre(c,"T")=="")continue
+		var c=Chr(cd1)+cd2,s=Qre(c,"T");if(s==" "||s=="")continue
 			for(i in cd8){var d=Crd(c,cd8[i])
-				if(Id(d)){var t=Qre(d,"T")
-					if(t!=Qre(c,"T")&&t!=" "&&t!=""&&t!="Z"&&!zb.contains(d))zb.push(c)
+				if(Id(d)){var t=Qre(d,"T");if(t==" "||t==""||t=="Z")continue
+					if(t!=s&&zb.indexOf(c)==-1){zb.push(c);break}
 				}
 			}
-	}Dft.Clr=0;console.log(zb);alert("")
-	for(i in zb){alert(zb[i]);Sym(zb[i],4)}if(zb.length>0)Rul();Dft.Clr=0
+	}Dft.Clr=0
+	for(i in zb){Sym(zb[i],4)}if(zb.length>0)Rul();Dft.Clr=0
 }
