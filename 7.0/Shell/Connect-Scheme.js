2d6602a573a88f901a371886195ca221eb2b98ec
fixiabis <benkao0000@gmail.com>
2016-09-23 20:21:07 +0800

    Update Shell

--- 7.0/Shell/Connect-Scheme.js
+++ 7.0/Shell/Connect-Scheme.js
@@ -9,6 +9,7 @@ function ExR(){Dft.Clr=1
 		}
 		for(cd1=65;cd1<74;cd1++){
 			for(cd2=1;cd2<10;cd2++){var c=Chr(cd1)+cd2
+				if(Qre(c,"B")=="royalblue"&&Qre(c,"T")=="O")Cln("O Win.","")
 				if(Qre(c,"B")=="crimson"&&Qre(c,"T")=="X")Cln("X Win.","")
 			}
 		}
@@ -16,4 +17,3 @@ function ExR(){Dft.Clr=1
 }
 function Adn(){bdB()}
 function Brd(){crB()}
-Cln("","")
