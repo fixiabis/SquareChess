19744788789e5df9850992eff2f43250fb79327c
fixiabis <benkao0000@gmail.com>
2016-09-24 16:27:36 +0800

    Update Connect.js

--- 7.0/Shell/Connect.js
+++ 7.0/Shell/Connect.js
@@ -37,4 +37,4 @@ function crR(){if(!Dft.Rnd)return
 	var R=[["E4","E6","D5","F5"],["D4","D6","F4","F6"]]
 	for(i in R){var p=Val(Rnd()*4);Set(R[i][p])}
 }
-function ExB(){}function ExR(){}Cln()
+function ExB(){}function ExR(){}
