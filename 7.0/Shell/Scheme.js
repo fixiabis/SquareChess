b34c7c8df47286fae4af457b270934b45ba1caa4
fixiabis <benkao0000@gmail.com>
2016-09-24 23:45:32 +0800

    Update Scheme.js

--- 7.0/Shell/Scheme.js
+++ 7.0/Shell/Scheme.js
@@ -13,10 +13,8 @@ function ExR(){var t=Qre(Hst.Crd[Turn],"T")
 	for(i in Ara[1]){var t=Qre(Ara[1][i],"T");if(t=="X")Xf++;if(t=="O")Os=1}
 	if(Turn%2==0&&Turn>2){
 		if(Os&&Xs&&Turn>4||Of==Ara[0].length&&Xf==Ara[1].length)Cln("Draw")
-		else if(Os)Cln("O Win.")
-		else if(Xf==Ara[1].length)Cln("O Win")
-		else if(Xs)Cln("X Win.")
-		else if(Of==Ara[0].length)Cln("X Win")
+		else if(Os||Xf==Ara[1].length)Cln("O Win")
+		else if(Xs||Of==Ara[0].length)Cln("X Win")
 	}OgC(Hst.Crd[Turn])
 }
 function ExL(c){if(Lt.indexOf(c)>-1&&Turn<2)return 1;else return 0}
