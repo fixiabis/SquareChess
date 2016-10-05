e51b98f36edc0d0ebbfb65d7f43697fd883ec77a
fixiabis <benkao0000@gmail.com>
2016-10-05 11:05:18 +0800

    Add files via upload

--- 7.1/Shell/Divider.js
+++ 7.1/Shell/Divider.js
@@ -9,18 +9,20 @@ function Lmt(c,s){var k=0,r=1;Dft.Fcd="";if(!s){s=Sbl[Tn%2];k=ExL(c)}
 		}
 	}return r
 }
-function Rul(){Scr={O:0,X:0,P:0}
-	for(var i=0;i<9;i++)for(cd1=65;cd1<74;cd1++){
-		for(cd2=1;cd2<10;cd2++){
-			var Os=0,Xs=0,c=Chr(cd1)+cd2
-			if(!i)Id(c).title=""
-			if(Qre(c,"T")!=""||Id(c).title!="")continue
-			for(n in cd8)if(Id(Crd(c,cd8[n]))){var t=Id(Crd(c,cd8[n])).title
-				if(t=="O"+(i-1))Os=1;if(t=="X"+(i-1))Xs=1
-			}if(!i){Os=!Lmt(c,"O");Xs=!Lmt(c,"X")}
-			if(Os&&Xs){Id(c).title="P"+i;Scr.P++}
-			if(Os&&!Xs){Id(c).title="O"+i;Scr.O++}
-			if(!Os&&Xs){Id(c).title="X"+i;Scr.X++}
+function Rul(){var a=0;Scr={O:0,X:0,P:0}
+	for(var i=0;i<9;i++){if(a>80)break
+		for(cd1=65;cd1<74;cd1++){if(a>80)break
+			for(cd2=1;cd2<10;cd2++){if(a>80)break
+				var Os=0,Xs=0,c=Chr(cd1)+cd2
+				if(!i)Id(c).title=""
+				if(Qre(c,"T")!=""||Id(c).title!=""){a++;continue}
+				for(n in cd8)if(Id(Crd(c,cd8[n]))){var t=Id(Crd(c,cd8[n])).title
+					if(t=="O"+(i-1))Os=1;if(t=="X"+(i-1))Xs=1
+				}if(!i){Os=!Lmt(c,"O");Xs=!Lmt(c,"X")}
+				if(Os&&Xs){Id(c).title="P"+i;Scr.P++}
+				if(Os&&!Xs){Id(c).title="O"+i;Scr.O++}
+				if(!Os&&Xs){Id(c).title="X"+i;Scr.X++}
+			}
 		}
 	}
 	if(Scr.P==0&&Tn%2==0){
