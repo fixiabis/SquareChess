4a9e5fc4a5306b336ee99e26d78b82811043f32d
fixiabis <benkao0000@gmail.com>
2016-10-06 22:03:43 +0800

    Add files via upload

--- 7.1/Shell/Follow.js
+++ 7.1/Shell/Follow.js
@@ -1,12 +1,11 @@
-Dft.Lmt=0
-function ExL(c){
-	if(!Dft.Lmt){Dft.Lmt=1;return XLt(c)}
+function Set(c){if(!Flw(c)){Hst.Crd[Tn+1]=c;Qre(c,"T",Sbl[Tn%2]);Tn++}Rul();Wtr()}
+function Flw(c){var k=Lmt(c)
+	if(!Hst.Crd[Tn-1]&&!k)return 0
+	if(Instr(Dft.Fcd,Hst.Crd[Tn-1])<0)return 1;return 0
 }
-function XLt(c){var r=0
-	if(!Hst.Crd[Tn-2]){Dft.Lmt=0;return 0}Lmt(c);Dft.Lmt=0
-	if(Instr(Dft.Fcd,Hst.Crd[Tn-1])<0)return 1
-	else return Spc(c)
-}
-function Spc(c){var p=0
-	for(cd1=65;cd1<74;cd1++)for(cd2=1;cd2<10;cd2++)if(!Lmt(Chr(cd1)+cd2,Sbl[Tn%2]))p++;if(!p){Cln(Sbl[(Sym(Sbl[Tn%2])+1)%2])}else return 0
+function ExR(){var s=0;if(Tn<2)return
+	for(cd1=65;cd1<74;cd1++)for(cd2=1;cd2<10;cd2++){
+		var f=Lmt(Chr(cd1)+cd2,Sbl[Tn%2])
+		if(Instr(Dft.Fcd,Hst.Crd[Tn-1])>-1)s++
+	}if(s==0)Cln(Sbl[(Tn+1)%2]+" Win")
 }
