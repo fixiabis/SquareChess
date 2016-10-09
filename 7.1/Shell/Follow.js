f2a5a0a4e58461a366d79b1f25b8d07376f94290
fixiabis <benkao0000@gmail.com>
2016-10-09 17:38:54 +0800

    Add files via upload

--- 7.1/Shell/Follow.js
+++ 7.1/Shell/Follow.js
@@ -4,7 +4,7 @@ function Flw(c){var k=Lmt(c)
 	if(Instr(Dft.Fcd,Hst.Crd[Tn-1])<0)return 1;return 0
 }
 function ExR(){var s=0;if(Tn<2)return
-	for(cd1=65;cd1<74;cd1++)for(cd2=1;cd2<10;cd2++){
+	for(cd1=65;cd1<74;cd1++)for(cd2=1;cd2<10;cd2++){if(Qre(Chr(cd1)+cd2,"T")=="")continue
 		var f=Lmt(Chr(cd1)+cd2,Sbl[Tn%2])
 		if(Instr(Dft.Fcd,Hst.Crd[Tn-1])>-1)s++
 	}if(s==0)Cln(Sbl[(Tn+1)%2]+" Win")
