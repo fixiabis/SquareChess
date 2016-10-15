c41b06aeeed53400440724103df2bb0acc5fb0b9
fixiabis <benkao0000@gmail.com>
2016-10-15 20:31:56 +0800

    Update

--- 7.1.1/Gear.js
+++ 7.1.1/Gear.js
@@ -1,8 +1,12 @@
 function Lgn(){var typ="J";if(Id("Jcd").value=="")typ="L"
 	$.get("https://script.google.com/macros/s/AKfycbzvZ4sL8J0e4cjN4fH_AjWLZr17sqQggkCpS60FLQwoJMgaKw/exec",
-		{Typ:typ,Act:Id("Act").value,Pwd:Id("Pwd").value,Jcd:Id("Jcd").value,Mod:"Connect"},
+		{Typ:typ,Act:Id("Act").value,Pwd:Id("Pwd").value,Jcd:Id("Jcd").value,Mod:location.search.split("?mode=")[1]},
 		function (r){
-			
+			if(Instr(r,"/")<0)alert(r)
+			else{var rtn=r.split("/'")
+				Dft.Usr=Id("Act").value;Dft.URw=rtn[0]
+				Dft.Jcd=rtn[1];alert("邀請代碼:"+Dft.Jcd)
+			}
 		}
 	)
 }
