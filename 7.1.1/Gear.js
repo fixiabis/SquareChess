96f1d48bf419ffff9538ec04ce10e1510ac3b936
fixiabis <benkao0000@gmail.com>
2016-10-15 22:41:33 +0800

    Add files via upload

--- 7.1.1/Gear.js
+++ 7.1.1/Gear.js
@@ -7,21 +7,31 @@ function Lgn(){var typ="J";if(Id("Jcd").value=="")typ="L"
 				if(typ=="L"){var rtn=r.split("/'")
 					Dft.Usr=Id("Act").value;Dft.URw=rtn[0];Dft.Jcd=rtn[1];alert("邀請代碼:"+Dft.Jcd)
 				}else{
-					Dft.Usr=Id("Act").value;Dft.URw=Val(r);Dft.Jcd=Id("Jcd").value;alert("加入成功")
+					Dft.Usr=Id("Act").value;Dft.URw=Val(r);Dft.Jcd=Id("Jcd").value;alert("加入成功");Get()
 				}Ldr()
 			}
 		}
 	)
 }
-function Upl(){
+function Upl(){if(Tn==0)return
 	for(cd1=65;cd1<74;cd1++)for(cd2=1;cd2<10;cd2++)Id(Chr(cd1)+cd2).onclick=function(){}
 	$.get("https://script.google.com/macros/s/AKfycbzvZ4sL8J0e4cjN4fH_AjWLZr17sqQggkCpS60FLQwoJMgaKw/exec",
-		{Typ:"S",Jcd:Dft.Jcd,Rw:Dft.URw,Brd:Hst.Brd[Tn]},
+		{Typ:"S",Jcd:Dft.Jcd,Rw:Dft.URw,Brd:Hst.Brd[Tn]+":"+Tn},
 		function (r){
-			alert(r)
+			if(r=="設置完成")Get();else alert(r)
 		}
 	)
 }
 function Get(){
-	
+	$.get("https://script.google.com/macros/s/AKfycbzvZ4sL8J0e4cjN4fH_AjWLZr17sqQggkCpS60FLQwoJMgaKw/exec",
+		{Typ:"R",Jcd:Dft.Jcd,Rw:Dft.URw},
+		function (r){var rtn=r.split(":");console.log(r)
+			if(rtn[1]!=Tn){Rdr(rtn[0]);Tn=Val(rtn[1])
+				for(cd1=65;cd1<74;cd1++)for(cd2=1;cd2<10;cd2++)Id(Chr(cd1)+cd2).onclick=function(){Set(this.id)}
+			}else setTimeout("Get()",2000)
+		}
+	)
 }
+function Udo(){}
+function Rdo(){}
+function Gto(){}
