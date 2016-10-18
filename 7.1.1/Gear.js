672c81f6fff7f753a7fe9d68c75194aa42cd4b5d
fixiabis <benkao0000@gmail.com>
2016-10-18 21:18:32 +0800

    Add files via upload

--- 7.1.1/Gear.js
+++ 7.1.1/Gear.js
@@ -6,8 +6,9 @@ function Lgn(){if(Dft.Lgn)return;var typ="J";if(Id("Jcd").value=="")typ="L";Dft.
 			if(Instr(r,"/")>-1){
 				if(typ=="L"){var rtn=r.split("/'")
 					Dft.Usr="Host";Dft.URw=rtn[0];Dft.Jcd=rtn[1];alert("邀請代碼:"+Dft.Jcd)
-				}else{
-					Dft.Usr="Join";Dft.URw=r.split("/")[1];Dft.Jcd=Id("Jcd").value;alert("加入成功")
+				}else{var rtn=r.split("/");Dft.Usr="Join";Dft.URw=rtn[1];Dft.Jcd=Id("Jcd").value
+					if(rtn[0]=="空間已有人使用"){Dft.Usr="View";alert("進入參觀模式")}
+					else alert("加入成功")
 				}Ldr()
 			}else{alert(r);Dft.Lgn=0;Id("Exc").style.backgroundColor="lightgray"}
 		}
@@ -18,7 +19,7 @@ function Upl(v){if(Tn==0||!Dft.Upl)return;EnS();BfS();var bd=Hst.Brd[Tn]+":"+Tn+
 	$.get(olsvr,
 		{Typ:"S",Jcd:Dft.Jcd,Rw:Dft.URw,Brd:bd},
 		function (r){
-			if(bd=="gvp"){Cln();if(Dft.Usr=="Host")EnS(1);else{EnS();Get()}}
+			if(bd=="gvp")Cln()
 			else if(r=="設置完成"){Rul();Get();Dft.Gvp=0}
 			else alert(r)
 		}
@@ -29,11 +30,11 @@ function Get(){
 		{Typ:"R",Jcd:Dft.Jcd,Rw:Dft.URw},
 		function (r){
 			if(r=="gvp"){
-				if(!Dft.Gvp){Cln("對方認輸");Dft.Gvp=1}if(Dft.Usr=="Host")EnS(1);else{EnS();Get()}
+				if(!Dft.Gvp){Cln("對方認輸");Dft.Gvp=1}
 			}else if(r!=""){Dft.Gvp=0
 				var rtn=r.split(":")
-				if(rtn[0]!=""&&rtn[1]==Tn+1){Tn=rtn[1]-1
-					EnS(1);Dft.Upl=0;Set(rtn[2]);Dft.Upl=1;BfS();Rdr(rtn[0]);Rul()
+				if(rtn[0]!=""&&rtn[1]!=Tn+1){Tn=rtn[1]-1
+					EnS(1);Dft.Upl=0;Set(rtn[2]);Dft.Upl=1;BfS();Rdr(rtn[0]);Rul();if(Usr=="View")Get()
 				}else Get()
 			}else Get()
 		}
@@ -61,7 +62,7 @@ function Cln(m,t){if(!m)m="";if(!t)t=""
 	if(m!="")alert(m);Tn=0;Hst={Brd:[],Crd:[]}
 	for(cd1=65;cd1<74;cd1++)for(cd2=1;cd2<10;cd2++){
 		if(Instr(Qre(Chr(cd1)+cd2,"T"),t)==-1)continue;Sym(Chr(cd1)+cd2,2)
-	}Brd();Wtr()
+	}Brd();Wtr();if(Dft.Usr=="Host")EnS(1);else{EnS();Get()}
 }
 function Set(c){if(!Dft.Set)return;if(!Lmt(c)){Hst.Crd[Tn+1]=c;Qre(c,"T",Sbl[Tn%2]);Tn++;Wtr()}}
 function KDw(e){if(e.which==13)Lgn()}
