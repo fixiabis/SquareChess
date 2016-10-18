e75e9fc190dd582cc1da29383c0467b06f06b19c
fixiabis <benkao0000@gmail.com>
2016-10-18 20:12:36 +0800

    Update

--- 7.1.1/Gear.js
+++ 7.1.1/Gear.js
@@ -49,7 +49,7 @@ function LMd(n){
 function Oln(){
 	$.get(olsvr,
 		{Typ:"O",Jcd:Dft.Jcd,Rw:Dft.URw,Usr:Dft.Usr},
-		function (r){console.log(r)
+		function (r){
 			if(r=="對方在線"||r=="尚未有人加入"){Dft.Uln=0;Oln()}
 			else if(Dft.Uln>3){
 				if(confirm(r+",是否繼續等待?")){Dft.Uln=0;Oln()}else location.reload()
