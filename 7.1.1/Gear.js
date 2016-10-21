cc9e9bdc249d7d7c4a2de7358312292e69f5ff26
fixiabis <benkao0000@gmail.com>
2016-10-21 16:49:33 +0800

    Add files via upload

--- 7.1.1/Gear.js
+++ 7.1.1/Gear.js
@@ -1,4 +1,5 @@
 ﻿var olsvr="https://script.google.com/macros/s/AKfycbzvZ4sL8J0e4cjN4fH_AjWLZr17sqQggkCpS60FLQwoJMgaKw/exec";Dft.Upl=1
+function Acn(){if(location.hash){Id("Act").value=location.hash.split("#")[1]}}
 function Lgn(){if(Dft.Lgn)return;var typ="J";if(Id("Jcd").value=="")typ="L";Dft.Lgn=1;Id("Exc").style.backgroundColor="dimgray"
 	$.get(olsvr,
 		{Typ:typ,Act:Id("Act").value,Pwd:Id("Pwd").value,Jcd:Id("Jcd").value,Mod:location.search.split("?mode=")[1]},
@@ -18,7 +19,7 @@ function Upl(v){if(!Dft.Upl)return;EnS();BfS();var bd=Hst.Brd[Tn]+":"+Tn+":"+Hst
 	$.get(olsvr,
 		{Typ:"S",Jcd:Dft.Jcd,Rw:Dft.URw,Brd:bd},
 		function (r){
-			if(bd=="gvp"){Dft.Gvp=1;Cln();if(Dft.Usr=="Host")EnS(1);else{EnS();Get()}}
+			if(bd=="gvp"){Dft.Gvp=1;Cln();Itl()}
 			else if(r=="設置完成"){Rul();Get();Dft.Gvp=0}
 			else alert(r)
 		}
@@ -29,16 +30,26 @@ function Get(){
 		{Typ:"R",Jcd:Dft.Jcd,Rw:Dft.URw},
 		function (r){
 			if(r=="gvp"){
-				if(!Dft.Gvp){Dft.Gvp=1;Cln("對方認輸")}if(Dft.Usr=="Host")EnS(1);else{EnS();Get()}
+				if(!Dft.Gvp){Dft.Gvp=1;Cln("對方認輸")}Itl()
 			}else if(r!=""){Dft.Gvp=0
 				var rtn=r.split(":")
 				if(rtn[0]!=""&&rtn[1]==Tn+1){Tn=rtn[1]-1
-					EnS(1);Dft.Upl=0;Set(rtn[2]);Dft.Upl=1;BfS();Rdr(rtn[0]);Rul()
+					EnS(1);Dft.Upl=0;Set(rtn[2]);Dft.Upl=1;Rdr(rtn[0]);Rul()
 				}else Get()
 			}else Get()
 		}
 	)
 }
+function Cln(m,t){if(!m)m="";if(!t)t=""
+	if(m!="")alert(m);Tn=0;Hst={Brd:[],Crd:[]}
+	for(cd1=65;cd1<74;cd1++)for(cd2=1;cd2<10;cd2++){
+		if(Instr(Qre(Chr(cd1)+cd2,"T"),t)==-1)continue;Sym(Chr(cd1)+cd2,2)
+	}Brd();Wtr();Itl()
+}
+function Itl(){if(Dft.Usr=="Host")EnS(1);else{EnS();Get()}}
+function Set(c){if(!Dft.Set)return;if(!Lmt(c)){Hst.Crd[Tn+1]=c;Qre(c,"T",Sbl[Tn%2]);Tn++;Wtr()}}
+function EnS(v){if(v)Dft.Set=1;else Dft.Set=0}
+function Gvp(){if(!Dft.Set)return;if(confirm("確定認輸?")){Upl("gvp")}}
 function LMd(n){
 	var s=doc.createElement("script")
 	s.src="Shell/"+LdM[n]+".js"
@@ -46,19 +57,19 @@ function LMd(n){
 	else s.onload=function(){Cre();Id("UC").innerHTML="<div class='bt' onClick='Gvp()'>GiveUp</div>";Cln();Rsz();if(Dft.Usr=="Join"){Get();EnS()}Oln()}
 	doc.body.appendChild(s)
 }
-function Oln(){}
-function Cln(m,t){if(!m)m="";if(!t)t=""
-	if(m!="")alert(m);Tn=0;Hst={Brd:[],Crd:[]}
-	for(cd1=65;cd1<74;cd1++)for(cd2=1;cd2<10;cd2++){
-		if(Instr(Qre(Chr(cd1)+cd2,"T"),t)==-1)continue;Sym(Chr(cd1)+cd2,2)
-	}Brd();Wtr();if(Dft.Usr=="Host")EnS(1);else{EnS();Get()}
+function KDw(e){var c=e.ctrlKey,k=e.which,s=e.shiftKey
+	switch(k){
+		case 13:Lgn();break
+		case 37:Hst.Dir+="L";break
+		case 38:Hst.Dir+="F";break
+		case 39:Hst.Dir+="R";break
+		case 40:Hst.Dir+="B";break
+		default:if(k>64&&k<74&&!c&&!Id("Act"))Set(Chr(k)+prompt("輸入座標"))
+	}
 }
-function Set(c){if(!Dft.Set)return;if(!Lmt(c)){Hst.Crd[Tn+1]=c;Qre(c,"T",Sbl[Tn%2]);Tn++;Wtr()}}
-function KDw(e){if(e.which==13)Lgn()}
-function EnS(v){if(v)Dft.Set=1;else Dft.Set=0}
-function Acn(){if(location.hash){Id("Act").value=location.hash.split("#")[1]}}
-function Gvp(){if(!Dft.Set)return;if(confirm("確定認輸?")){Upl("gvp")}}
+function Oln(){}
 function Udo(){}
 function Rdo(){}
 function Gto(){}
 function Clr(){}
+function ToS(){}
