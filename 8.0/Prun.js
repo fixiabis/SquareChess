eb9904b8662d71551d4f67a4a831008a39043fc9
fixiabis <benkao0000@gmail.com>
2016-11-04 22:21:18 +0800

    Update

---
+++ 8.0/Prun.js
@@ -0,0 +1,9 @@
+var doc=document
+function Asc(v){return v.charCodeAt()}
+function Chr(v){return String.fromCharCode(v)}
+function Val(v){return v*1}
+function Rnd(){return Math.random()}
+function Mid(v,s,e){return v.substr(s,e)}
+function Id(v){return doc.getElementById(v)}
+function Class(v){return doc.getElementsByClassName(v)}
+function Tag(v){return doc.getElementsByTagName(v)}
