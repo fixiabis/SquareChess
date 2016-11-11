64ae921f6bfd0cce88f83e72bbc3b148f9b000fd
fixiabis <benkao0000@gmail.com>
2016-11-11 20:54:42 +0800

    Update

--- 8.0/Aide.js
+++ 8.0/Aide.js
@@ -1,5 +1,7 @@
-function MsO(){
-	
+function MsO(e){
+	if(e.pageX<Id("Board").offsetLeft)Mnu(1)
+	else if(e.pageX>Id("Board").offsetLeft+Id("Board").offsetWidth)Mnu(1,1)
+	else Mnu()
 }//滑鼠移動
 function KDw(e){
 	
@@ -7,6 +9,11 @@ function KDw(e){
 function KUp(e){
 	
 }//按鍵放開
-function Mnu(v){var h=0;if(v)h=160;
+function Mnu(v,p){var h=0;if(v)h=160;
 	Id("menu").style.width=h+"px"
+	if(p){Id("menu").style.left=""
+		Id("menu").style.right="0px"
+	}else{Id("menu").style.left="0px"
+		Id("menu").style.right=""
+	}
 }//輔助選單
