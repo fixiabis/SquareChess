41865d5d788597dd350febbcf855c2a364e8f166
fixiabis <benkao0000@gmail.com>
2016-11-11 21:03:18 +0800

    Update

--- 8.0/Aide.js
+++ 8.0/Aide.js
@@ -1,7 +1,8 @@
-function MsO(e){
-	if(e.pageX<Id("Board").offsetLeft)Mnu(1)
-	else if(e.pageX>Id("Board").offsetLeft+Id("Board").offsetWidth)Mnu(1,1)
-	else Mnu()
+function MsO(e){var v=1,p=0
+	if(e.pageX<Id("Board").offsetLeft)p=0
+	else if(e.pageX>Id("Board").offsetLeft+Id("Board").offsetWidth)p=1
+	else v=0
+	Mnu(v,p)
 }//滑鼠移動
 function KDw(e){
 	
@@ -11,7 +12,7 @@ function KUp(e){
 }//按鍵放開
 function Mnu(v,p){var h=0;if(v)h=160;
 	Id("menu").style.width=h+"px"
-	if(p){Id("menu").style.left=""
+	if(v)if(p){Id("menu").style.left=""
 		Id("menu").style.right="0px"
 	}else{Id("menu").style.left="0px"
 		Id("menu").style.right=""
