0f841a0cabf6e61c880621a62863ee001f15f04c
fixiabis <benkao0000@gmail.com>
2016-11-15 22:20:21 +0800

    Add files via upload

--- 8.0/Mode/ByLine.js
+++ 8.0/Mode/ByLine.js
@@ -22,39 +22,22 @@ Shl.OpK.ByLine=function(){
 	Ara.OpK("ByLine")
 }
 function Lin(){var ara={O:[],X:[]};ara.O.age=[];ara.X.age=[]
-	for(var cd1=65;cd1<74;cd1++)for(cd2=1;cd2<10;cd2++){var key=Qre(Chr(cd1)+cd2,"Sym");if(key==2||cd1==72&&cd2==9)continue
-		for(var i=1;i<10;i++){var cds=["B","R","BR"]
-			for(var j=0;j<3;j++){
-				cds[j]=Crd(Chr(cd1)+cd2,i+cds[j])
-				if(Qre(cds[j],"Sym")!=key)
-			}
-		}
-		for(var c1=cd1+1;c1<74;c1++){if(Qre(Chr(c1)+cd2,"Sym")!=key)continue
-			var a=Sel(Chr(cd1)+":"+Chr(c1)+cd2),age=[Chr(cd1)+cd2,Chr(c1)+cd2]
-			for(var i=0;i<2;i++)age[i]=Hst.Crd.indexOf(age[i])
-			var yng=Math.max(age[0],age[1]),old=Math.min(age[0],age[1])
-			for(var i=0;i<a.length-1;i++){
-				if(Qre(a[i],"Sym")==2){var p=ara[Sqr.Sym[key]].indexOf(a[i])
-					if(p<0){
-						ara[Sqr.Sym[key]].push(a[i]);ara[Sqr.Sym[key]].age.push([old,yng])
-					}else{
-						if(ara[Sqr.Sym[key]].age[p][0]>old)ara[Sqr.Sym[key]].age[p][0]=old
-						if(ara[Sqr.Sym[key]].age[p][1]>yng)ara[Sqr.Sym[key]].age[p][1]=yng
-					}
-				}
-			}
-		}
-		for(var c2=cd2+1;c2<10;c2++){if(Qre(Chr(cd1)+c2,"Sym")!=key)continue
-			var a=Sel(Chr(cd1)+cd2+":"+c2),age=[Chr(cd1)+cd2,Chr(cd1)+c2]
-			for(var i=0;i<2;i++)age[i]=Hst.Crd.indexOf(age[i])
-			var yng=Math.max(age[0],age[1]),old=Math.min(age[0],age[1])
-			for(var i=0;i<a.length-1;i++){
-				if(Qre(a[i],"Sym")==2){var p=ara[Sqr.Sym[key]].indexOf(a[i])
-					if(p<0){
-						ara[Sqr.Sym[key]].push(a[i]);ara[Sqr.Sym[key]].age.push([old,yng])
-					}else{
-						if(ara[Sqr.Sym[key]].age[p][0]>old)ara[Sqr.Sym[key]].age[p][0]=old
-						if(ara[Sqr.Sym[key]].age[p][1]>yng)ara[Sqr.Sym[key]].age[p][1]=yng
+	for(var cd1=65;cd1<74;cd1++)for(cd2=1;cd2<10;cd2++){
+		var key=Qre(Chr(cd1)+cd2,"Sym");if(key==2||cd1==72&&cd2==9)continue
+		for(var c=1;c<10;c++){
+			var cds=[Chr(cd1+c)+cd2,Chr(cd1)+(cd2+c),Chr(cd1+c)+(cd2+c),Chr(cd1-c)+(cd2+c)]
+			for(var s=0;s<4;s++){if(Qre(cds[s],"Sym")!=key)continue
+				var a=Sel(Chr(cd1)+cd2+"~"+cds[s]),age=[Chr(cd1)+cd2,cds[s]]
+				for(var i=0;i<2;i++)age[i]=Hst.Crd.indexOf(age[i])
+				var yng=Math.max(age[0],age[1]),old=Math.min(age[0],age[1])
+				for(var i=0;i<a.length-1;i++){
+					if(Qre(a[i],"Sym")==2){var p=ara[Sqr.Sym[key]].indexOf(a[i])
+						if(p<0){
+							ara[Sqr.Sym[key]].push(a[i]);ara[Sqr.Sym[key]].age.push([old,yng])
+						}else{
+							if(ara[Sqr.Sym[key]].age[p][0]>old)ara[Sqr.Sym[key]].age[p][0]=old
+							if(ara[Sqr.Sym[key]].age[p][1]>yng)ara[Sqr.Sym[key]].age[p][1]=yng
+						}
 					}
 				}
 			}
