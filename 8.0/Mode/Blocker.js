202381d28cb1f8b5c055227ac0a7d50c4f5dc63a
fixiabis <benkao0000@gmail.com>
2016-11-15 16:28:50 +0800

    Add files via upload

--- 8.0/Mode/Blocker.js
+++ 8.0/Mode/Blocker.js
@@ -1,6 +1,6 @@
 Shl.Ara.Blocker=[];Dft.Blocker={Ara:0,QJd:1,AJd:0}
-Shl.Rul.Blocker=function(){Shl.Ara.Blocker=Blk()
-	
+Shl.Rul.Blocker=function(){
+	return Ara.Rul("Blocker",Blk())
 }
 Shl.Lmt.Blocker=function(crd,sym){}
 Shl.Mrk.Blocker=function(){Ara.Mrk("Blocker")}
@@ -45,15 +45,15 @@ function Blk(){var ara={O:[],X:[]};ara.O.age=[];ara.X.age=[]
 	}var sot={O:{All:[]},X:{All:[]},P:{All:[]}}
 	for(var cd1=65;cd1<74;cd1++){
 		for(var cd2=1;cd2<10;cd2++){if(Qre(Chr(cd1)+cd2,"Sym")!=2)continue
-			var sym=[ara.O.indexOf(Chr(cd1)+cd2),ara.X.indexOf(Chr(cd1)+cd2)]
+			var crd=Chr(cd1)+cd2,sym=[ara.O.indexOf(crd),ara.X.indexOf(crd)]
 			for(var i=0;i<2;i++){var ck=0
 				if(sym[i]>-1&&sym[(i+1)%2]<0)ck=1
 				else if(sym[i]>-1&&sym[(i+1)%2]>-1)switch(Dft.Blocker.AJd){
 					case 0:if(ara[Sqr.Sym[i]].age[sym[i]][0]<ara[Sqr.Sym[(i+1)%2]].age[sym[(i+1)%2]][0])ck=1;break
 					case 1:if(ara[Sqr.Sym[i]].age[sym[i]][1]>ara[Sqr.Sym[(i+1)%2]].age[sym[(i+1)%2]][1])ck=1;break
 					case 2:ck=0
-				}if(ck)sot[Sqr.Sym[i]].All.push(Chr(cd1)+cd2)
-			}
+				}if(ck)sot[Sqr.Sym[i]].All.push(crd)
+			}if(sot.O.All.indexOf(crd)<0&&sot.X.All.indexOf(crd)<0)sot.P.All.push(crd)
 		}
-	}return sot
+	}sot.O[0]=sot.O.All;sot.X[0]=sot.X.All;sot.P[0]=sot.P.All;return sot
 }
