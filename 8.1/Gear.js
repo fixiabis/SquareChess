b18935c7c0ebcd1265b827801dc58916249fdf32
fixiabis <benkao0000@gmail.com>
2016-11-26 14:08:49 +0800

    Update

--- 8.1/Gear.js
+++ 8.1/Gear.js
@@ -1,4 +1,4 @@
-﻿var Oln={}
+﻿var Oln={},ckt=0
 function Req(Typ,Jcd){var id="",req={ModeName:location.search.split("?mode=")[1],LastActive:new Date().getTime(),BoardContent:""}
 	if(Jcd)id=Jcd
 	if(Typ=="J"){if(!Jcd)while(!id)id=prompt("輸入id");Dft.Oln.Typ="X"}
@@ -23,23 +23,23 @@ function Upl(cnt){if(Dft.Oln.Typ=="V")return
 	try{firebase.database().ref("Battle/"+Dft.Oln.Id).update(req);Dft.Oln.Cln=1}
 	catch(e){if(confirm("暫時無法上傳，將繼續重試"))Upl(cnt)}
 }
-function Ini(){Dft.System.Oln=0;Cln();Dft.System.Oln=1;Dft.Oln.Cln=0
+function Ini(v){Dft.System.Oln=0;Cln();Dft.System.Oln=1;Dft.Oln.Cln=0
 	if(Dft.Oln.Typ=="X"||Dft.Oln.Typ=="V")Dft.Set=0;else Dft.Set=1
-	firebase.database().ref("Battle/"+Dft.Oln.Id).on("value",function(r){
+	if(!v)firebase.database().ref("Battle/"+Dft.Oln.Id).on("value",function(r){
 		if(r.val().Message&&Id("msgc").innerHTML!=r.val().Message){Id("msgc").innerHTML=r.val().Message;Ctl("MSw",1)}
 		if(Dft.Oln.Typ!="V"){var d=new Date().getTime()
 			firebase.database().ref("Battle/"+Dft.Oln.Id).update({LastActive:d});if(Dft.Oln.CkK)Oln.Ckr(d,1)
 		}
 		var brd=r.val().BoardContent.split("/")
-		if(brd[0].length<81&&Dft.Oln.Cln){alert(brd[0]);Ini()}
+		if(brd[0].length<81&&Dft.Oln.Cln){alert(brd[0]);Ini(1)}
 		else if(brd[1]&&Sqr.Sym[(Val(brd[1])%2)]==Dft.Oln.Typ||Dft.Oln.Typ=="V"){
 			Hst.Brd[brd[1]]=brd[0];Hst.Crd[brd[1]]=brd[2];Rec(brd[0]);Tn=brd[1];Rul()
 			if(Dft.Oln.Typ!="V"){Dft.Set=1;doc.title="輪到你下了"}
 		}
 	})
 }
-Oln.Ckr=function(d,s){if(s)return setTimeout("Oln.Ckr("+d+")",Dft.Oln.CkS*1000)
-	firebase.database().ref("Battle/"+Dft.Oln.Id).once("value",function(r){
+Oln.Ckr=function(d,s){console.log(ckt);if(s){setTimeout("Oln.Ckr("+d+")",Dft.Oln.CkS*1000);ckt++;return}
+	firebase.database().ref("Battle/"+Dft.Oln.Id).once("value",function(r){ckt--
 		if(r.val().LastActive==d&&r.val().PlayerX){
 			if(Dft.Oln.Typ=="O"){
 				if(confirm("對方已不在線,是否要更新房間?"))
@@ -67,7 +67,7 @@ Oln.Opt=function(){Id("msgr").style.opacity=0
 Oln.OpK=function(){
 	if(!Dft.Oln.Id){if(Id("Dft-ORg-0").checked)Req("R");else Req("J")}
 	if(Id("Dft-Oln-CkS")){
-		if(Id("Dft-Oln-CkS").value<1)Dft.Oln.CkS=1;else Dft.Oln.CkS=Id("Dft-Oln-CkS").value
+		if(Id("Dft-Oln-CkS").value<5)Dft.Oln.CkS=5;else Dft.Oln.CkS=Id("Dft-Oln-CkS").value
 		Dft.Oln.CkK=Id("Dft-Oln-CkK").checked
 	}
 }
