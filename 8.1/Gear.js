987f77359244a1b9aa06dd69321307e4329c37c3
fixiabis <benkao0000@gmail.com>
2016-11-27 16:29:48 +0800

    Update

--- 8.1/Gear.js
+++ 8.1/Gear.js
@@ -64,3 +64,6 @@ function RJC(){var r="",t=[]
   for(i=0;i<8;i++)r+=t[Math.floor(Math.random()*36)]
   return r
 }
+function BkC(){var r=""
+	for(var i=0;i<10;i++)r+=Math.floor(Math.random()*10);return r
+}
