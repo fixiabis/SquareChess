c30e4e0bb28c1be46e3b69d15f7ff5f86588442c
fixiabis <benkao0000@gmail.com>
2016-11-28 13:59:54 +0800

    Add files via upload

--- 8.1/Mode/GoLike.js
+++ 8.1/Mode/GoLike.js
@@ -1,10 +1,12 @@
 Shl.Ara.GoLike=[];Dft.GoLike={MJg:0}
-Shl.Rul.GoLike=function(){var ara=[],cds=Sel("All"),vst=[]
+Shl.Rul.GoLike=function(){var ara=[],cds=Sel("All"),vst=[],ckr=[0,0];if(Tn<2)return
 	for(var i=0;i<cds.length;i++){
 		if(Dft.GoLike.MJg)odr=Idt;else odr=function(map){if(map.indexOf(2)<0)return 1;return 0}
 		if(vst.indexOf(cds[i])>-1||Qre(cds[i],"Sym")==2)continue
 		var map=Map(cds[i]);if(Map.Bdr(map,1,odr)||Map.Bdr(map,0,odr))ara.push(map);vst=vst.concat(map)
 	}for(var i=0;i<ara.length;i++)Qre(ara[i],"Sym",4)
+	for(var i=0;i<cds.length;i++){var sym=Qre(cds[i],"Sym");ckr[sym]++}
+	if(!ckr[0]&&!ckr[1])return "平手";for(var i=0;i<2;i++)if(ckr[i]==0)return Enm(Sqr.Sym[i])+"獲勝"
 }
 Shl.Lmt.GoLike=function(){}
 Shl.Mrk.GoLike=function(){
