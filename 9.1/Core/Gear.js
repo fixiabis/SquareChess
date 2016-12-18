e58c37ff6db60cee25915aca390bd60d95b20635
fixiabis <benkao0000@gmail.com>
2016-12-19 00:29:41 +0800

    Add files via upload

--- 9.1/Core/Gear.js
+++ 9.1/Core/Gear.js
@@ -74,8 +74,7 @@ Oln.Opt=function(){Id("msgr").style.opacity=0
 		OpS("Dft.ORg-1/Dft.ORg","r","加入房間",Dft.Oln.Rgt==1)
 	}else{if(Tn<2)Id("msgr").style.opacity=1
 		Id("OptionMenu").innerHTML+="<input type='text' readonly value='"+Dft.Oln.Id+"' style='font-size:inherit;width:140px;text-align:center'/><br>"
-		OpS("Oln-MSw","k","訊息窗彈出",Dft.Oln.MSw)
-	}
+	}OpS("Oln-MSw","k","訊息窗彈出",Dft.Oln.MSw)
 }
 Oln.OpK=function(){
 	if(!Dft.Oln.Id){if(Id("Dft-ORg-0").checked)Req("R");else Req("J")}
